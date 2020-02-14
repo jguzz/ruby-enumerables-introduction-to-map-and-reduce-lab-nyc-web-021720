98c3a3f065ff61a4b44b87e7f6fe8b0e65c35599
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 03:32:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,14 +43,3 @@ def reduce_to_total(source_array, starting_point = 0)
   end
   total
 end
-
-def reduce_to_all_true(source_array)
-  i = 0
-  while i < source_array.length
-    if source_array[i] == true
-      i += 1
-    else
-     return false
-  end
-  true
-end
